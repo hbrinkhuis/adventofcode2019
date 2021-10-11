ffbf6ed08f4f755debba461f175740b2c2d0c9da
Henri Brinkhuis <hbrinkhuis@obi4wan.com>
2021-10-11 13:19:09 +0200

    Continuing old code!

---
+++ AdventOfCode2019/DayThreeTakeTwo.fs
@@ -0,0 +1,24 @@
+﻿module DayThreeTakeTwo
+
+    open Plotly.NET
+
+    let parse_wire wire =
+        let xData = [0]
+        let yData = [0]
+        0
+
+    let parse_wires wires =
+        wires |> List.map parse_wire
+
+    let charts wires =
+        
+        parse_wires wires
+        let xData = [2.;4.;6.;8.;10.]
+        let yData = xData
+
+        let x = 
+            [
+                Chart.Line(xData, yData) 
+                //Chart.Line(negate xData, negate yData)
+            ] |> Chart.combine |> Chart.show
+        0
--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -5,19 +5,24 @@
 
     [<EntryPoint>]
     let main argv =
-        
-        // day one
-        let module_weights = readlines @"Resources\day1.txt" |> Seq.map int |> List.ofSeq
-        printfn "Day one puzzle one answer: %i" (DayOne.part_one module_weights)
-        printfn "Day one puzzle two answer: %i" (DayOne.part_two module_weights)
 
-        // day two
-        let opcodes = readlines @"Resources\day2.txt" |> Seq.map (fun x -> x.Split(',')) |> Seq.concat |> Seq.map int |> Array.ofSeq
-        printfn "Day two puzzle one answer: %i" (DayTwo.part_one opcodes)
-        let opcodes = readlines @"Resources\day2.txt" |> Seq.map (fun x -> x.Split(',')) |> Seq.concat |> Seq.map int |> Array.ofSeq
-        printfn "Day two puzzle two answer: %A" (DayTwo.part_two opcodes 19690720)
+        //// day one
+        //let module_weights = readlines @"Resources\day1.txt" |> Seq.map int |> List.ofSeq
+        //printfn "Day one puzzle one answer: %i" (DayOne.part_one module_weights)
+        //printfn "Day one puzzle two answer: %i" (DayOne.part_two module_weights)
+
+        //// day two
+        //let opcodes = readlines @"Resources\day2.txt" |> Seq.map (fun x -> x.Split(',')) |> Seq.concat |> Seq.map int |> Array.ofSeq
+        //printfn "Day two puzzle one answer: %i" (DayTwo.part_one opcodes)
+        //let opcodes = readlines @"Resources\day2.txt" |> Seq.map (fun x -> x.Split(',')) |> Seq.concat |> Seq.map int |> Array.ofSeq
+        //printfn "Day two puzzle two answer: %A" (DayTwo.part_two opcodes 19690720)
 
         // day three
         let wires = readlines @"Resources\day3.txt" |> Seq.map (fun x -> x.Split(',') |> List.ofArray ) |> List.ofSeq
         printfn "Day three puzzle one answer: %i" (DayThree.part_one wires)
-        0
+
+        let list = [1 .. 10]
+
+        printfn "The sum of the list is %A" (List.scan(fun x state -> x * state) 1 list)
+
+        DayThreeTakeTwo.charts wires
